2089e8f49061b5f8e976f23d777ea28117571219
StKondrat <kndrt13@icloud.com>
2026-03-01 22:48:21 +0300

    Design interface for BiList

--- main.cpp
+++ main.cpp
@@ -1,2 +1,38 @@
+#include <cstddef>
+
+template< class T >
+struct BiList
+{
+  T val;
+  BiList<T> * next;
+  BiList<T> * prev;
+};
+
+
+template< class T >
+BiList<T> * fake(BiList<T> * h);
+
+template< class T >
+BiList<T> * rmfake(BiList<T> * h) noexcept;
+
+template< class T >
+BiList<T> * add(BiList<T> * h, const T & data);
+
+template< class T >
+BiList<T> * instrt(BiList<T> * h, const T & data);
+
+template< class T >
+BiList<T> * cut(BiList<T> * h) noexcept;
+
+template< class T >
+BiList<T> * erase(BiList<T> * h) noexcept;
+
+template< class T >
+BiList<T> * clear(BiList<T> * h, BiList<T> * e) noexcept;
+
+template< class T, class F >
+F traverse(F f, BiList<T> * h, BiList<T> * e);
+
+
 int main()
 {}
